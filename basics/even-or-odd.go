d87c7a1b8b1030b267cd7f383facdc350056c107
Wanderley Mayhé Junior <Juniormayhe@gmail.com>
2019-07-19 11:23:33 +0100

    Update even-or-odd.go

--- basics/even-or-odd.go
+++ basics/even-or-odd.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello, 世界")
-	numbers := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println("Check if numbers are even or odd")
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	for _, number := range numbers {
 
