1366f99060a51cb65416bfd03a98924ccaae8e78
Wanderley Mayhé Junior <Juniormayhe@gmail.com>
2019-07-19 11:18:41 +0100

    Create even-or-odd.go

---
+++ basics/even-or-odd.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello, 世界")
+	numbers := []int{1, 2, 3, 4, 5, 6}
+
+	for _, number := range numbers {
+
+		result := "odd"
+
+		if number%2 == 0 {
+			result = "even"
+		}
+
+		fmt.Println(number, "is", result)
+	}
+}
